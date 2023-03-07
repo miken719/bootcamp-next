a1cecd9fb16c39ba607c56389d277d5da2b5d1ad
Miken Prajapati <miken.prajapati@indianic.com>
2023-03-07 14:03:28 +0530

    dynamic loading

---
+++ component/home/homepage.js
@@ -0,0 +1,65 @@
+import { useFormik } from "formik";
+import { useRouter } from "next/router";
+
+const HomePage = () => {
+  const router = useRouter();
+  const exploreBootcampFormik = useFormik({
+    initialValues: {
+      miles: "",
+      zipcode: "",
+    },
+    onSubmit: (values) => {
+      if (values.miles === "" || values.zipcode === "") {
+        router.push("/bootcamp");
+      } else {
+        router.push(`/bootcamp/${values.zipcode}/${values.miles}`);
+      }
+    },
+  });
+  return (
+    <section className="showcase">
+      <div className="dark-overlay">
+        <div className="showcase-inner container">
+          <h1 className="display-4">Find a Code Bootcamp</h1>
+          <p className="lead">
+            Find, rate and read reviews on coding bootcamps
+          </p>
+
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="miles"
+                  placeholder="Miles From"
+                  value={exploreBootcampFormik.values.miles}
+                  onChange={exploreBootcampFormik.handleChange}
+                />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="zipcode"
+                  placeholder="Enter Zipcode"
+                  value={exploreBootcampFormik.values.zipcode}
+                  onChange={exploreBootcampFormik.handleChange}
+                />
+              </div>
+            </div>
+          </div>
+          <input
+            type="submit"
+            onClick={() => exploreBootcampFormik.handleSubmit()}
+            defaultValue="Find Bootcamps"
+            className="btn btn-primary btn-block"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default HomePage;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Header from "@/component/Layout/Header";
 import { googleEvent } from "@/component/utils/googleAnalytics";
-import { useFormik } from "formik";
-import { useRouter } from "next/router";
+
+import dynamic from "next/dynamic";
 import { useEffect } from "react";
+const HomePage = dynamic(() => import("@/component/home/homepage"));
 
 export default function Home() {
-  const router = useRouter();
-
   useEffect(() => {
     googleEvent({
       event_category: "Home (Banner)",
@@ -14,67 +13,12 @@ export default function Home() {
     });
   }, []);
 
-  const exploreBootcampFormik = useFormik({
-    initialValues: {
-      miles: "",
-      zipcode: "",
-    },
-    onSubmit: (values) => {
-      if (values.miles === "" || values.zipcode === "") {
-        router.push("/bootcamp");
-      } else {
-        router.push(`/bootcamp/${values.zipcode}/${values.miles}`);
-      }
-    },
-  });
   return (
     <div>
       {/* Navbar */}
       <Header />
       {/* Showcase */}
-      <section className="showcase">
-        <div className="dark-overlay">
-          <div className="showcase-inner container">
-            <h1 className="display-4">Find a Code Bootcamp</h1>
-            <p className="lead">
-              Find, rate and read reviews on coding bootcamps
-            </p>
-
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="miles"
-                    placeholder="Miles From"
-                    value={exploreBootcampFormik.values.miles}
-                    onChange={exploreBootcampFormik.handleChange}
-                  />
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="zipcode"
-                    placeholder="Enter Zipcode"
-                    value={exploreBootcampFormik.values.zipcode}
-                    onChange={exploreBootcampFormik.handleChange}
-                  />
-                </div>
-              </div>
-            </div>
-            <input
-              type="submit"
-              onClick={() => exploreBootcampFormik.handleSubmit()}
-              defaultValue="Find Bootcamps"
-              className="btn btn-primary btn-block"
-            />
-          </div>
-        </div>
-      </section>
+      <HomePage />
     </div>
   );
 }
