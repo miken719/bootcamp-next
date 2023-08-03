2c768424118b20e04262762274d08fd8627c2aa4
Miken Prajapati <miken.prajapati@indianic.com>
2023-08-03 18:36:46 +0530

    ui change

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
+
         <meta name="robots" content="noindex,nofollow" />
         <meta httpEquiv="dns-prefetch-control" content="on" />
 
