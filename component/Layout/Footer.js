f1d445efca55b91041330ceefec00839706ac683
Miken Prajapati <miken.prajapati@indianic.com>
2023-08-07 15:19:25 +0530

    ui change

--- component/Layout/Footer.js
+++ component/Layout/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer bg-primary fixed-bottom">
-      <div className="footer__made-by">Made with ❤️ by Miken</div>
+      <div className="footer__made-by">Made by Miken</div>
     </footer>
   );
 };
