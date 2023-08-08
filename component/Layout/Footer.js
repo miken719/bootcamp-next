ffdf91748197f4c052a33138994e19c71fa19b1c
Miken Prajapati <miken.prajapati@indianic.com>
2023-08-08 17:59:17 +0530

    seo tags changes

--- component/Layout/Footer.js
+++ component/Layout/Footer.js
@@ -1,7 +1,28 @@
+import React from "react";
+import Image from "next/image"; // Import the Image component from Next.js
+
 const Footer = () => {
   return (
     <footer className="footer bg-primary fixed-bottom">
       <div className="footer__made-by">Made by Miken</div>
+      <div className="footer__social-links">
+        <a
+          href="https://twitter.com/mikenprajapati"
+          target="_blank"
+          title="Twitter"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+        <a
+          href="https://github.com/mikenindianic"
+          target="_blank"
+          title="Github"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </div>
     </footer>
   );
 };
